73e45757ccfbfda06cef674c8343578d009ab318
JunSuHeo <wmfkf95@gmail.com>
2020-07-03 14:52:12 +0900

    유저 가이드 추가

---
+++ app/src/main/java/com/example/ringtest/FragmentThird.java
@@ -0,0 +1,17 @@
+package com.example.police_fishing;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentThird extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.guide_3p, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/ringtest/UserGuide.java
@@ -0,0 +1,200 @@
+package com.example.police_fishing;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.ViewCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import me.relex.circleindicator.CircleIndicator3;
+
+public class UserGuide extends FragmentActivity {
+    private static final int NUM_PAGES = 3;
+    private ViewPager2 viewPager;
+    private FragmentStateAdapter pagerAdapter;
+    private CircleIndicator3 mIndicator;
+    private Button finish_btn;
+
+    private boolean is_first;
+    private Context mContext;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_guide);
+        checkSelfPermission();
+
+        mContext = this;
+
+        is_first = PreferenceManager.getBoolean(mContext, "is_first");
+
+        finish_btn = findViewById(R.id.btn_finish);
+
+        finish_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                PreferenceManager.setBoolean(mContext, "is_first", false);
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // Instantiate a ViewPager2 and a PagerAdapter.
+        viewPager = findViewById(R.id.viewpager);
+        pagerAdapter = new ScreenSlidePagerAdapter(this);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setPageTransformer(new ZoomOutPageTransformer());
+
+        mIndicator = findViewById(R.id.indicator);
+        mIndicator.setViewPager(viewPager);
+        mIndicator.createIndicators(NUM_PAGES,0);
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                if (positionOffsetPixels == 0) {
+                    viewPager.setCurrentItem(position);
+                }
+            }
+
+            // 페이지 선택되어있는 부분 확인
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                mIndicator.animatePageSelected(position%NUM_PAGES);
+                if(position == NUM_PAGES - 1){
+                    finish_btn.setVisibility(View.VISIBLE);
+                }
+                else{
+                    finish_btn.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        //가이드를 끝냈을때
+        if(!is_first){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //권한을 허용 했을 경우
+        if(requestCode == 1){
+            int length = permissions.length;
+            for (int i = 0; i < length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    // 동의
+                    Log.d("MainActivity","권한 허용 : " + permissions[i]);
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (viewPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+        }
+    }
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+
+        public ScreenSlidePagerAdapter(FragmentActivity fa) {
+            super(fa);
+        }
+
+        // Fragment 생성부분
+        @Override
+        public Fragment createFragment(int position) {
+            if(position == 0){
+
+                return new FragmentFirst();
+            }
+            else if(position == 1){
+
+                return new FragementSecond();
+            }
+            else{
+                return new FragmentThird();
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
+
+    }
+
+    public void checkSelfPermission() {
+        String temp = "";
+        //파일 읽기 권한 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.MANAGE_OWN_CALLS) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.MANAGE_OWN_CALLS + " ";
+        }
+        //파일 쓰기 권한 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.READ_CALL_LOG + " ";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.READ_PHONE_STATE + " ";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.VIBRATE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.VIBRATE + " ";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.SEND_SMS + " ";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.RECEIVE_SMS + " ";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.WRITE_CONTACTS + " ";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.READ_CONTACTS + " ";
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.FOREGROUND_SERVICE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.FOREGROUND_SERVICE + " ";
+        }
+        if (TextUtils.isEmpty(temp) == false) {
+            // 권한 요청
+            ActivityCompat.requestPermissions(this, temp.trim().split(" "),1);
+        }
+        else {
+            // 모두 허용 상태
+            Toast.makeText(this, "권한을 모두 허용", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
