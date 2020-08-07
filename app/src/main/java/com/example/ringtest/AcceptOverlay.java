02b0d08e9c5489622c69ceef4dc49a4d6af5ade4
KIMYONGGEE <ykkim6872@gmail.com>
2020-08-08 00:38:07 +0900

    설정창 확인 팝업
    설정창 확인 팝업

---
+++ app/src/main/java/com/example/ringtest/AcceptOverlay.java
@@ -0,0 +1,100 @@
+package com.example.ringtest;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Build;
+import android.os.IBinder;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import static java.lang.Thread.sleep;
+
+/***
+ *  통화 화면 위에 알람을 띄움
+ *
+ */
+
+public class AcceptOverlay extends Service {
+    WindowManager wm;
+    View mView;
+    @Override
+    public IBinder onBind(Intent intent) { return null; }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //sendNotification();
+
+        Log.d("OverlayService", "오버레이 시작");
+        LayoutInflater inflate = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        // 오레오 이상부터는 Type_Application_Overlay를 써야함
+        int LAYOUT_FLAG;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        } else {
+            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        }
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                LAYOUT_FLAG,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        |WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                        |WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL; // 레이아웃 위치 조정
+        //mView = inflate.inflate(R.layout.activity_overlay, null);
+        mView = inflate.inflate(R.layout.activity_overlay, null);
+
+
+        final TextView textView = mView.findViewById(R.id.accepttext2);
+
+        final Vibrator vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+
+        //진동
+        if (Build.VERSION.SDK_INT >= 29) {
+            vibrator.vibrate(VibrationEffect.createOneShot(1000, 70));
+        }
+
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //bt.setImageResource(R.mipmap.ic_launcher_round);
+                stopSelf();
+
+            }
+        });
+        wm.addView(mView, params);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("OverlayService", "오버레이 종료");
+
+        if(wm != null) {
+            if(mView != null) {
+                wm.removeView(mView);
+                mView = null;
+            }
+            wm = null;
+        }
+    }
+}
