e1c94212dd622489b2b77c8da9248a9e6d1ff740
SeokLii <k1nder@naver.com>
2020-08-23 16:38:54 +0900

    add user Guide

---
+++ app/src/main/java/com/example/ringtest/FragmentEight.java
@@ -0,0 +1,18 @@
+package com.example.ringtest;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentEight extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.guide_8p, container, false);
+
+
+        return rootView;
+    }
+}
